02f314552cdbbed675e206466fa836a832d5f652
Luka Barth <lukadbarth@hotmail.com>
2024-10-30 19:20:57 -0300

    curso segundo modulo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
 </script>
 
 <template>
   <Banner />
+  <ConteudoPrincipal />
 </template>
 
 <style scoped>
